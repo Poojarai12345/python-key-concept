5d84d72ffd5c1ee988b3abcf74b6e27cf4e23ffc
poojarai12345 <poojarai7637@gmail.com>
2022-04-18 14:39:12 +0530

    second.py-commit

---
+++ loops.py
@@ -0,0 +1,15 @@
+print(range(1,10))
+print(tuple(range(1,10)))
+print(tuple(range(1,10,2)))
+print(tuple(reversed(range(1,10))))
+print(tuple(range(10,1,-1)))
+
+
+
+for x in range(1,10):
+    print("China",x)
+
+x=1
+while (x<10):
+    print("India")
+    x+=1
