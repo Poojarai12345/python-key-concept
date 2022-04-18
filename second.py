1a2711544397daede0c1248c6b74759e23cbfa7c
poojarai12345 <poojarai7637@gmail.com>
2022-04-18 11:21:24 +0530

    second.py-commit

---
+++ second.py
@@ -0,0 +1,15 @@
+a = 20
+if a>20:
+    print("a is greater than 20")
+elif a<10:
+    print("less than 20")
+else:
+    print("a is greater or equal to 20")
+
+if (len("RAhul")>20):
+    print("String more than 20")
+elif(len("Rahul")>10):
+    print("String greater than 10")
+else:
+    print("String less than 10")
+    
