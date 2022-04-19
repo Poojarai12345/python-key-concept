d7793a6a84fa5f32cd26137fde51a0f5e4128519
poojarai12345 <poojarai7637@gmail.com>
2022-04-19 09:09:09 +0530

    function.py-commit

---
+++ function.py
@@ -0,0 +1,18 @@
+def fun():
+    print("doing this....")
+
+result=fun()
+print(result)
+
+def fun():
+    print('Explore more')
+
+def fun(a,b):
+    print(a,b)
+fun(35,54)
+fun('Alia','Menu')
+
+
+# same function can be created multiple time but only the last function would be taken 
+# if function does not return anything you will none as the return value 
+# if function has parameter you must pass them.
