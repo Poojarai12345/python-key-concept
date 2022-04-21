1b86ed4ae36d2e092eedce3b62beaf3262a446cd
poojarai12345 <poojarai7637@gmail.com>
2022-04-21 22:44:46 +0530

    explanation.py-commit

---
+++ explanation.py
@@ -0,0 +1,15 @@
+def temp(listm,x):
+    listm.append(x)
+
+lista=[23,34,653,345,234]
+temp=(lista,456)
+
+print(lista)
+
+def add(a,b):
+    a=(a+b)
+
+a1=(10)
+add(a1,50)
+print(a1)
+ 
---
+++ functionall.py
@@ -0,0 +1,32 @@
+#Python Creating Function
+def fun():
+    print('Great World')
+
+#Python calling function
+def fun():
+    print('Great World')
+
+fun()
+# Python Function with arguments
+#whether the number passed as an argument to the function is even or odd.
+
+num = int(input('Enter any number: '))
+
+if (num%2) == 0:
+    print('Even')
+else:
+    print('odd')
+
+#Keyword arguments
+
+def student(firstname,lastname):
+    print(firstname,lastname)
+
+student(firstname='Pooja', lastname= 'Rai')
+student(firstname='Greek', lastname='Alia')
+
+
+
+
+
+
