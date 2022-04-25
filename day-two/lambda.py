17449240faab37936ef20477ab05508af74f2920
poojarai12345 <poojarai7637@gmail.com>
2022-04-25 15:42:24 +0530

    lambda.py-commit

---
+++ day-two/lambda.py
@@ -0,0 +1,13 @@
+def add (a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+sum =lambda a,b: (a+b)
+minus=lambda a,b: (a-b)
+
+print(add(33,344))
+print(sub(344,34))
+print(sum(35,355))
+print(minus(355,35))
